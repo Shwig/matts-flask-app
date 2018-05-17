6a29bfca86ae7aa4f63313a8a8121bccf56f3325
Matthew O'Mara <omara.matthew1@gmail.com>
2018-05-16 23:56:35 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+## produce a random image
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/random_pic')
+def pic():
+    return render_template('app_pic.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
