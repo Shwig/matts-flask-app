6bc41c74a04d851a6460c574a4aa4b3fc54e65c3
omara <omara.matthew1@gmail.com>
2018-08-26 02:28:05 -0400

    page addition and layout fix

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def index():
 def pic():
     return render_template('app_pic.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
